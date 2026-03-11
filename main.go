7a2b75318a858bb144ed5e976f194c9f5e9f7c22
Gyreth <espindolatomas20@gmail.com>
2026-03-11 16:59:42 -0600

    libparalogin

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"ProyectoGinBack/controllers"
 	"ProyectoGinBack/middlewares"
 
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +16,15 @@ func main() {
 	config.MigrarModelos()
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
